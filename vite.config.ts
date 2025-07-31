51e5dd90416d056cfce73a1aff2fb55f870dfd3b
Patrik Björn <patrik.bjorn@fortnox.se>
2025-07-31 16:04:57 +0200

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/fortnox-inhouse-tournament/',
+    base: './',
     plugins: [
         tailwindcss(),
     ],
